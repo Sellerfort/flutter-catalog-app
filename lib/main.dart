75e8cdff36092398f87e2cef8829e2889c605fd9
Ruksar Ahmed <ruksar.org@gmail.com>
2021-05-28 17:33:13 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sellerfort/home_page.dart';
+import 'package:sellerfort/pages/home_page.dart';
+import 'package:sellerfort/pages/login_page.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -9,7 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
